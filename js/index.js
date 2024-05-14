0cbd97f0c86755b9f7991cdb7ce175a1995a314e
ibrhmvamin <aminibrahimov52@gmail.com>
2024-05-14 23:04:00 +0400

    jstasks

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const body = document.querySelector("body");
+
+// function createProductCard(name, price, url) {
+//   const pCard = `
+//   <div class="container">
+//       <h2>Name : ${name}</h2>
+//       <p>Price : ${price}$</p>
+//       <img width=300 height=200 src="${url}">
+//   </div>`;
+//   return pCard;
+// }
+
+// const div = createProductCard(
+//   "BMW",
+//   "100000$",
+//   "https://www.bmw.ca/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/navigation/bmw-8series-coupe-modellfinder.png"
+// );
+// body.insertAdjacentHTML("afterbegin", div);
+
+// function createCommentMessage(author, message, timestamp) {
+//   const comment = `
+//   <div class="container">
+//       <h2>Name : ${author}</h2>
+//       <p>Message : ${message}</p>
+//       <p>Date : ${timestamp}</p>
+//   </div>`;
+//   return comment;
+// }
+
+// const comment = createCommentMessage("Amin", "Salam", new Date());
+// body.insertAdjacentHTML("afterbegin", comment);
+
+// function createMenuItem(name, price, description) {
+//   const item = `
+//   <div class="container">
+//       <h2>Name : ${name}</h2>
+//       <p>Price : ${price}</p>
+//       <p>Description : ${description}</p>
+//   </div>`;
+//   return item;
+// }
+
+// const item = createMenuItem("Burger", "10$", "Special Burger");
+// body.insertAdjacentHTML("afterbegin", item);
